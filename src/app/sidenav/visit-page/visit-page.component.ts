18227298f7f246ae1c33eeb928fa1b963139c7c7
Pawel <pawel.tomasz.jur@gmail.com>
2019-05-30 14:59:40 +0200

    Z pracy

--- src/app/sidenav/visit-page/visit-page.component.ts
+++ src/app/sidenav/visit-page/visit-page.component.ts
@@ -1,15 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AccordionItem } from './examination-accordion/examination-accordion.component';
 import { Visit } from 'src/app/api/models';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+
+interface VisitForm {
+    description: string;
+    diagnosis: string;
+}
 
 @Component({
     selector: 'app-visit-page',
     templateUrl: './visit-page.component.html',
     styleUrls: ['./visit-page.component.scss'],
 })
-export class VisitPageComponent {
+export class VisitPageComponent implements OnInit {
     @Input()
     visit: Visit;
+
+    visitForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.visitForm = this.fb.group({
+            description: [null, Validators.required],
+            diagnosis: [null, Validators.required],
+        });
+    }
 
     availableTests: string[] = ['Badanie TSH', 'Roentgen', 'Rezonans magnetyczny'];
     prescribedTests: AccordionItem[] = [{ SelectedValue: '', Note: '' }];
@@ -19,4 +36,8 @@ export class VisitPageComponent {
         'Nasłuchiwanie płuc',
     ];
     examinations: AccordionItem[] = [{ SelectedValue: '', Note: '' }];
+
+    onSubmit(form: VisitForm): void {
+        console.log(form);
+    }
 }
