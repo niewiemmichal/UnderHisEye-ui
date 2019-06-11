1afaded31b6908f8e81ef39a2867f6041aa98bc7
Pawel <pawel.tomasz.jur@gmail.com>
2019-06-11 15:12:49 +0200

    Filled past exams with data

--- src/app/sidenav/past-lab-exams/past-lab-exams.component.ts
+++ src/app/sidenav/past-lab-exams/past-lab-exams.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LaboratoryExamination } from 'src/app/api/models';
+import { LaboratoryExaminationsService } from 'src/app/api/services';
+import { ColumnInfoItem } from 'src/app/shared/components/table/table.component';
 
 @Component({
     selector: 'app-past-lab-exams',
@@ -6,7 +9,61 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./past-lab-exams.component.scss'],
 })
 export class PastLabExamsComponent implements OnInit {
-    constructor() {}
+    private _labExaminations: LaboratoryExamination[] = [];
+    private _examFilters: ((exam: LaboratoryExamination) => boolean)[] = [];
 
-    ngOnInit() {}
+    columns: ColumnInfoItem[] = [
+        {
+            columnDef: 'patient',
+            header: 'Patient',
+            cell: (exam: LaboratoryExamination) =>
+                `${exam.visit.patient.name} ${exam.visit.patient.surname}`,
+        },
+        {
+            columnDef: 'exam',
+            header: 'Exam',
+            cell: (exam: LaboratoryExamination) =>
+                `${exam.examination.code} ${exam.examination.name}`,
+        },
+        {
+            columnDef: 'status',
+            header: 'Status',
+            cell: (exam: LaboratoryExamination) => exam.status.toLowerCase(),
+        },
+        {
+            columnDef: 'orderDate',
+            header: 'Order Date',
+            cell: (exam: LaboratoryExamination) => exam.orderDate,
+        },
+        {
+            columnDef: 'approveDate',
+            header: 'Approval Date',
+            cell: (exam: LaboratoryExamination) => exam.approvalDate,
+        },
+        {
+            columnDef: 'completeDate',
+            header: 'Completion Date',
+            cell: (exam: LaboratoryExamination) => exam.completionDate,
+        },
+    ];
+
+    constructor(private _labService: LaboratoryExaminationsService) {}
+
+    ngOnInit() {
+        this._labService
+            .getAllLaboratoryExaminationsUsingGET()
+            .subscribe((labExaminations: LaboratoryExamination[]) => {
+                this._labExaminations = labExaminations;
+            });
+    }
+
+    get labExaminations(): LaboratoryExamination[] {
+        return this._labExaminations.filter((labExamination: LaboratoryExamination) => {
+            return this._examFilters
+                .map((examFitler: (exam: LaboratoryExamination) => boolean) => {
+                    return examFitler(labExamination);
+                })
+                .every((value: boolean) => value === true);
+        });
+    }
 }
