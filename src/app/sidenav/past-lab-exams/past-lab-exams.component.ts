3fa925f6d5a3072bb3f8f9c1d28645140d078d56
Paweł Jur <pawel.tomasz.jur@gmail.com>
2019-06-11 22:47:08 +0200

    Added filtering options for past visits

--- src/app/sidenav/past-lab-exams/past-lab-exams.component.ts
+++ src/app/sidenav/past-lab-exams/past-lab-exams.component.ts
@@ -12,7 +12,6 @@ import { ExamDetailsDialogComponent } from './exam-details-dialog/exam-details-d
 })
 export class PastLabExamsComponent implements OnInit {
     private _labExaminations: LaboratoryExamination[] = [];
-    private _examFilters: ((exam: LaboratoryExamination) => boolean)[] = [];
 
     columns: ColumnInfoItem[] = [
         {
@@ -48,6 +47,10 @@ export class PastLabExamsComponent implements OnInit {
             cell: (exam: LaboratoryExamination) => exam.approvalDate,
         },
     ];
+    filterStatusOptions: string[] = ['ALL', 'FINISHED', 'APPROVED', 'CANCELED', 'REJECTED'];
+    filterStatus: 'ALL' | 'FINISHED' | 'APPROVED' | 'CANCELED' | 'REJECTED' = 'ALL';
+    filterName: string = '';
+    filterSurname: string = '';
 
     constructor(private _labService: LaboratoryExaminationsService, private _dialog: MatDialog) {}
 
@@ -55,18 +58,19 @@ export class PastLabExamsComponent implements OnInit {
         this._labService
             .getAllLaboratoryExaminationsUsingGET()
             .subscribe((labExaminations: LaboratoryExamination[]) => {
-                this._labExaminations = labExaminations;
+                this._labExaminations = labExaminations.filter(
+                    (labExaminations: LaboratoryExamination) => labExaminations.status !== 'ORDERED'
+                );
             });
     }
 
     get labExaminations(): LaboratoryExamination[] {
-        return this._labExaminations.filter((labExamination: LaboratoryExamination) => {
-            return this._examFilters
-                .map((examFitler: (exam: LaboratoryExamination) => boolean) => {
-                    return examFitler(labExamination);
-                })
-                .every((value: boolean) => value === true);
-        });
+        return this._labExaminations.filter(
+            (labExamination: LaboratoryExamination) =>
+                (this.filterStatus === 'ALL' || labExamination.status === this.filterStatus) &&
+                labExamination.visit.patient.name.toLowerCase().includes(this.filterName) &&
+                labExamination.visit.patient.surname.toLowerCase().includes(this.filterSurname)
+        );
     }
 
     selectedRow(selectedExam: LaboratoryExamination): void {
